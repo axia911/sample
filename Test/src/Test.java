227fdcc00453100d675cd644a3a7bd011505b274
GOODEE <GOODEE@DESKTOP-EH7OFHQ>
2021-01-20 13:58:55 +0900

    a

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,10 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("B");
-=======
->>>>>>> branch 'main' of https://github.com/axia911/sample
 		System.out.println("c");
 	}
 
