0306357991ae52a9f4d7acadc264236cb0606c41
문재훈 <loveocto@naver.com>
2021-01-25 11:27:39 +0900

    하고싶다 하고시퍼!!!!!!!!!!

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,12 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("311111111111111111");
-		System.out.println("BB");
-		System.out.println("Hi");
-		System.out.println("YO");
-		System.out.println("hiihi");
-		System.out.println("뭐해야됨");
 		System.out.println("31");
 		System.out.println("A");
 		System.out.println("A");
@@ -19,12 +13,9 @@ public class Test {
 		System.out.println("나도나도 하고싶어 !!!!!!!!!!!!");
 		System.out.println("머지툴 두번쨰");
 		System.out.println("머지툴 두번쨰");
+		System.out.println("왜 난 안되는겨?????");
 		
-	
-		System.out.println("머지툴");
-		
-		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
-		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
+	}
 		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
 	}
 
