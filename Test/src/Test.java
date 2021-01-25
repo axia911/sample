702fa594479ba1d24b35772015101d7ec62e9780
박은희 <ehp7320@naver.com>
2021-01-25 12:03:52 +0900

    ㅁㅁㅁ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -5,17 +5,9 @@ public class Test {
 		System.out.println("Hi!나누구게");
 		System.out.println("1");
 		System.out.println("BBCDEFG");
-<<<<<<< HEAD
-		System.out.println("ABCCC");
-		System.out.println("지연2133");
-		System.out.println("재힝~2");
-		System.out.println("ABCDE");
-		
-=======
 		System.out.println("ABC");
 		System.out.println("~정리중~");
 
->>>>>>> branch 'main' of https://github.com/axia911/sample
 	}
 
 }
