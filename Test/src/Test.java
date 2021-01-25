a315e491a034b2267614f13129bb58a727f5d8db
박상원 <aksangwon@gmail.com>
2021-01-25 11:22:22 +0900

    ㄴㄴ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,20 +3,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("311111111111111111");
-<<<<<<< HEAD
 		System.out.println("A");
 		System.out.println("2");
 		System.out.println("2");
 		System.out.println("2");
-=======
-		
+	
 
 		System.out.println("BBCDEFG");
 		System.out.println("ABC");
 		System.out.println("Hi my name is jhn");
 		System.out.println("머지툴");
 		
->>>>>>> branch 'main' of https://github.com/axia911/sample
 	}
 
 }
