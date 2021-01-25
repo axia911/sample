ac610bd2af2bdbe1f540cb48d0f0389db71d300e
배재현 <sky73462@naver.com>
2021-01-25 11:29:18 +0900

    .

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -6,7 +6,10 @@ public class Test {
 		System.out.println("1");
 		System.out.println("BBCDEFG");
 		System.out.println("ABC");
-		System.out.println("재힝11");
+		System.out.println("재힝1");
+		System.out.println("재힝22");
+		System.out.println("재힝333");
+		System.out.println("재힝4444");
 		
 	}
 
