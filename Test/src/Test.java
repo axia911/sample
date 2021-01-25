6e51653ccd9899ab6b4bae4527201625ae3d28e1
문재훈 <loveocto@naver.com>
2021-01-25 11:50:22 +0900

    ????

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,12 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("~떡복이~");
-		System.out.println("~되는겨???~");
-=======
 		System.out.println("~떡복이순대~");
->>>>>>> branch 'main' of https://github.com/axia911/sample
+		System.out.println("~되는겨???~");
 	}
 
 }
