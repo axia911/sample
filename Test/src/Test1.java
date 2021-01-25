80278a5ec1cea018eb75eb0c11fc4d2173565310
장혜린 <hrjang26@naver.com>
2021-01-25 12:11:06 +0900

    1

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -2,14 +2,6 @@
 public class Test1 {
 
 	public Test1() {
-		System.out.println("조요요오옹!");
-<<<<<<< HEAD
-		
-
-		System.out.println("파일도 올라가나?");
->>>>>>> branch 'main' of https://github.com/axia911/sample
-=======
-
->>>>>>> branch 'main' of https://github.com/axia911/sample
+		System.out.println("1!");
 	}
 }
