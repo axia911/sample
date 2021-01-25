e90c20c464e9df290794a459610262b42aa77b19
장혜린 <hrjang26@naver.com>
2021-01-25 11:37:51 +0900

    1

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,15 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hi!");		
-		System.out.println("Hi!나누구게");
-		System.out.println("Hi!나누구게"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("1");
-		System.out.println("BBCDEFG");
-		System.out.println("ABC");
-		System.out.println("지연2133");
-		System.out.println("재힝~2");
-		System.out.println("ABCDE");
 		
 	}
 
