d187cec5932e6ee8e67ee3f8cca20eb89e1b6806
양수영 <adgjl0122@naver.com>
2021-01-25 11:30:00 +0900

    COMMIT

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,14 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("1");
 		System.out.println("BBCDEFG");
-<<<<<<< HEAD
-		System.out.println("YSY");
-		System.out.println("머지툴해보고시퍼충돌충돌!!!!");
-		System.out.println("머지툴 두번쨰");
-=======
 		System.out.println("ABC");
 		System.out.println("재힝11");
->>>>>>> branch 'main' of https://github.com/axia911/sample
 		
 	}
 
