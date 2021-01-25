8c4af277324596c00a9f477a297df1f80e43c327
유지연 <somyz@naver.com>
2021-01-25 11:09:55 +0900

    000309

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("31");
+		System.out.println("000309");
 		System.out.println("A");
 		System.out.println("Hi");
 	}
