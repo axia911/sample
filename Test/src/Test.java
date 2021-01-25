f783839be26c66cd815766ed994b04303a9185c2
조진형 <whwlsgud111@naver.com>
2021-01-25 11:11:21 +0900

    111

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,9 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("311111111111111111");
+		System.out.println("31");
 		System.out.println("A");
 		System.out.println("Hi");
+		System.out.println("i");
 	}
 
 }
