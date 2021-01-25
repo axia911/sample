50d8dde1f52537be19a35a92a0d908e0204b25b4
재힝 <shairs23@naver.com>
2021-01-25 11:19:48 +0900

    123

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,16 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("31");
-		System.out.println("A");
-
-		System.out.println("A");
-		System.out.println("A");
-		
 		System.out.println("1");
 		System.out.println("BBCDEFG");
 		System.out.println("ABC");
-		System.out.println("머지툴");
+		System.out.println("재힝");
 		
 	}
 
