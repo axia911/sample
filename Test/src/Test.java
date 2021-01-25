351242209f77345db06d2b70817255204bfed901
장현나 <wkdgs95@naver.com>
2021-01-25 11:10:23 +0900

    jhn

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,8 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("311111111111111111");
 		System.out.println("BB");
-		System.out.println("Hi");
+		System.out.println("Hi my name is jhn");
 		System.out.println("뭐해야됨");
+		
 	}
 
 }
