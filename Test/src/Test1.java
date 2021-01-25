c6bf6c23c82a94956e67e6d4727175eae5934573
배재현 <sky73462@naver.com>
2021-01-25 12:21:40 +0900

    .

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -1,27 +1,4 @@
 
 public class Test1 {
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-	public Test1() {
-		System.out.println("파일도 올라가나?");
-		System.out.println("새로운거네");
-		System.out.println("예압");
-		System.out.println("헤헿");
-		System.out.println("여긴 사람없니?");
-		System.out.println("재후닝~");
-		System.out.println("문재훈 멍청이");
-		System.out.println("????");
-		System.out.println("형 잤죠?");
-<<<<<<< HEAD
-=======
-		System.out.println("하하");
-		System.out.println("배고프다");
-		System.out.println("아뭐야 왜자꾸 터져");
->>>>>>> branch 'main' of https://github.com/axia911/sample
-=======
-
-	}
->>>>>>> branch 'main' of https://github.com/axia911/sample
 }
