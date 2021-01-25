adf765d0b8c26ee6b3b38942460456b86900c892
문재훈 <loveocto@naver.com>
2021-01-25 11:19:24 +0900

    터려랏!!!!!!!!!!!!!!!!!!!

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,17 +3,19 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("311111111111111111");
+
 		System.out.println("BB");
 		System.out.println("Hi");
 		System.out.println("YO");
 		System.out.println("hiihi");
 		System.out.println("뭐해야됨");
+		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
+
 		System.out.println("ABC");
 		System.out.println("Hi my name is jhn");
 		System.out.println("머지툴");
-	
-	
-		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
+		
+
 	}
 
 }
