95374546b470d1951b5f2791f03b2dec8ea5e56a
문재훈 <loveocto@naver.com>
2021-01-25 11:15:10 +0900

    터져랏!!!!!!!!!!!

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -8,8 +8,7 @@ public class Test {
 		System.out.println("YO");
 		System.out.println("hiihi");
 		System.out.println("뭐해야됨");
-	
-		System.out.println("나도 하고싶어 !!!!!!!!!!!!");
+		System.out.println("아아아아아악 나도 하고싶어 !!!!!!!!");
 	}
 
 }
