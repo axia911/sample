90cdadf2d389b9838fb137991edbe0c8ba6e3109
허도영 <gjqkdlqjf@gmail.com>
2021-01-25 11:39:05 +0900

    허허

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,12 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("~허허~");
-=======
-		System.out.println("Hi!");		
-		
->>>>>>> branch 'main' of https://github.com/axia911/sample
 	}
 
 }
