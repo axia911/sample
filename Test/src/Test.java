14f5882383bb1dd3ec180c17112c22f4c09c75cb
손대호 <daehoson@naver.com>
2021-01-25 12:06:09 +0900

    ㅇㄴㅇㄴ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,10 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
-		System.out.println("오늘 찾을 수 있을까?");
-		System.out.println("오늘 찾을 수 있을까?");
-		System.out.println("오늘 찾을 수 있을까?");
+		\
+		System.out.println("다시");
 	}
 
 }
