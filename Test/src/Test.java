253d260c792ec05d2b7697b6131fc68a7551f81a
이채연 <lcy2390@naver.com>
2021-01-25 11:31:17 +0900

    32121

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("31");
+		System.out.println("311");
 		System.out.println("A");
 		System.out.println("A");
 		System.out.println("A");		
