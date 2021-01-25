d9fc9f396dc79d655f23f9f289f5049fec81e85f
양수영 <adgjl0122@naver.com>
2021-01-25 11:25:24 +0900

    YSY 수정

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -8,7 +8,7 @@ public class Test {
 		System.out.println("A");		
 		System.out.println("1");
 		System.out.println("BBCDEFG");
-		System.out.println("ABC");
+		System.out.println("YSY");
 		System.out.println("머지툴해보고시퍼충돌충돌!!!!");
 		System.out.println("머지툴 두번쨰");
 		
