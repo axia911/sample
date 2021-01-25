a40d7deb72e45bfd786cf14c7bb1f3b106d39684
손대호 <daehoson@naver.com>
2021-01-25 11:44:03 +0900

    ㅇㄴㅁㅇㄴㅁ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,29 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("Hi!나누구게");
-		System.out.println("Hi!나누구게"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("나도 좀 올려보자"); 
-		System.out.println("1");
-		System.out.println("BBCDEFG");
-		System.out.println("ABC");
-		System.out.println("오늘의 점심은 떡뽁이");
-		System.out.println("재힝~2");
-		System.out.println("ABCDE");
-		
-=======
-		System.out.println("~허허~");
->>>>>>> branch 'main' of https://github.com/axia911/sample
-=======
-		System.out.println("~문문문~");
->>>>>>> branch 'main' of https://github.com/axia911/sample
+		System.out.println("~떡복이~");
 	}
 
 }
