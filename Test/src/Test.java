2a2ebc4db4e2fe1a2cf233964dabd1dd0bde48c3
엄 <aksgdma12@naver.com>
2021-01-25 11:41:48 +0900

    가나다라마바사

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,7 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("~허허~");
+		System.out.println("2");
+		System.out.println("F");
+		System.out.println("Hi");
 	}
 
 }
