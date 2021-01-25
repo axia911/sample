0b4ebdc3daf1e6959ad8ade45fbef096b9f394b7
정우진 <skd03244@naver.com>
2021-01-25 11:09:54 +0900

    45654654

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("311111111111111111");
+		System.out.println("34254456464");
 		System.out.println("A");
 		System.out.println("Hi");
 	}
