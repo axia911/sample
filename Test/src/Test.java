6034f705fb062544a1cb82c8b6cd15a525694690
김지연 <kimaa36@naver.com>
2021-01-25 11:12:49 +0900

    hi

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("31");
 		System.out.println("A");
-		System.out.println("Hi");
+		System.out.println("Hi~");
 	}
 
 }
