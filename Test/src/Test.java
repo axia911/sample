fc606a02f04ab8cdac33fcb6cc406953613d2a82
손대호 <daehoson@naver.com>
2021-01-25 12:03:21 +0900

    ㅇㄴㅇㄴㅇㄴ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -21,6 +21,8 @@ public class Test {
 		
 		System.out.println("그래서 현나의 스페이스바는 어디에?");
 		System.out.println("오늘 찾을 수 있을까?");
+		System.out.println("오늘 찾을 수 있을까?");
+		System.out.println("오늘 찾을 수 있을까?");
 	}
 
 }
