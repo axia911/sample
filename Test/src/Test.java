fadb6c3a1ca4980329d94e4155bb8f528f198350
장혜린 <hrjang26@naver.com>
2021-01-25 11:17:51 +0900

    1

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,9 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("311111111111111111");
-		
-
+		System.out.println("1");
 		System.out.println("BBCDEFG");
 		System.out.println("ABC");
 		System.out.println("머지툴");
