05904e8c3f751b8dab52cff1c9a895b975dd68a6
장혜린 <hrjang26@naver.com>
2021-01-25 11:59:04 +0900

    1

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,12 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("~떡복이순대~");
-		System.out.println("~떡튀순~");
-
-		System.out.println("Hi!3");
-		System.out.println("Hi!4");
-
+		System.out.println("Hi!");
 	}
 
 }
