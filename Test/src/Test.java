c0463961e7dd507c93eec105fe24eef97d3484a4
장현나 <wkdgs95@naver.com>
2021-01-25 11:52:30 +0900

    ㅋ

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,17 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hi!나누구게");
-		System.out.println("1");
-		System.out.println("BBCDEFG");
-		System.out.println("ABC");
-		System.out.println("~정리중~");
-		System.out.println("시러시러~");
-
-		System.out.println("~허허~");
-		System.out.println("~시러시러~");
-		
-		System.out.println("~허허~");
+		System.out.println("pull 어케함");
 	}
 
 }
