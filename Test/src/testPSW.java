83ada49e4741491f320f680bdad433ca4874cd4a
이대우 <Leejws9753@gmail.com>
2021-01-25 12:00:51 +0900

    ㅋㅋ

--- Test/src/testPSW.java
+++ Test/src/testPSW.java
@@ -7,6 +7,11 @@ public class testPSW {
 		System.out.println("우");
 		System.out.println("에");
 		System.out.println("오");
+		System.out.println("가");
+		System.out.println("나");
+		System.out.println("다");
+		System.out.println("라");
+		System.out.println("마");
 	}
 
 }
