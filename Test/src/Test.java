10636bced5ae4fa2cdd3c508eb953701c6837ccb
재힝 <shairs23@naver.com>
2021-01-25 11:37:01 +0900

    힌트

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,13 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hi!");
 		System.out.println("Hi!나누구게"); 
 		System.out.println("장현나지 누구긴 누구야"); 
 		System.out.println("1");
 		System.out.println("BBCDEFG");
 		System.out.println("ABC");
-<<<<<<< HEAD
 		System.out.println("재힝ddddd11ㅗㅗ");
 		System.out.println("재힝1");
 		System.out.println("재힝22");
@@ -24,11 +22,6 @@ public class Test {
 		System.out.println("4.정수기");
 		System.out.println("5.본체 뒤");
 		
-=======
-		System.out.println("지연2133");
-		System.out.println("재힝~2");
-		System.out.println("ABCDE");
->>>>>>> branch 'main' of https://github.com/axia911/sample
 		
 	}
 
