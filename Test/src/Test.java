de031340d906dede2c8631bf5e61eeda6be44e02
유지연 <somyz@naver.com>
2021-01-25 12:15:48 +0900

    정리

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -5,15 +5,7 @@ public class Test {
 		System.out.println("다시");
 
 		System.out.println("Hi!");
-		System.out.println("스페이스바 힌트는");
-		System.out.println("어디 있을까");
-		System.out.println("1.강사님자리");
-		System.out.println("2.책상 밑");
-		System.out.println("3.사물함");
-		System.out.println("4.정수기");
-		System.out.println("5.본체 뒤");
-		System.out.println("6.없음");
-		System.out.println("그래서 현나의 스페이스바는 어디에?");
-		System.out.println("오늘 찾을 수 있을까?OK?");
+		System.out.println("=정리=");
+
 	}
 }
