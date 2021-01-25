3ebffa93951c2a9f45b3841ad1fafd327eff4e8f
엄 <aksgdma12@naver.com>
2021-01-25 11:45:06 +0900

    A

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("F");
+		System.out.println("A");
 		System.out.println("Hi");
 	}
 
