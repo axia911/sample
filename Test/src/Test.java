99fa612a44921ac61bb3d9014491e09263e885f7
이대우 <Leejws9753@gmail.com>
2021-01-25 11:07:42 +0900

    BB

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("311111111111111111");
-		System.out.println("A");
+		System.out.println("BB");
 		System.out.println("Hi");
 	}
 
