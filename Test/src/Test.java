da3d3071ab0efc07c2ee9aeb077427e8f6fe0f85
이채연 <lcy2390@naver.com>
2021-01-25 11:53:07 +0900

    222

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,14 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("~떡복이순대~");
-		System.out.println("~되는겨???~");
-=======
 
-		System.out.println("Hi!");
+		System.out.println("Hi!2");
 
->>>>>>> branch 'main' of https://github.com/axia911/sample
 	}
 
 }
