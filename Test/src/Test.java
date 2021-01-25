a0685e687a4dd4df27459e9a159792b875ab4e16
손대호 <daehoson@naver.com>
2021-01-25 11:04:16 +0900

    ds\dkgkgk

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("31");
 		System.out.println("A");
+
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
+		System.out.println("A");
 		System.out.println("Hi");
 	}
 
