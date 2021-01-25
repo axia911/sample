b6347105ea2b8f85411207251ef682d950b157f9
유지연 <somyz@naver.com>
2021-01-25 11:14:27 +0900

    mmmm

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -2,13 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("000309");
-		System.out.println("A");
-=======
 		System.out.println("311111111111111111");
 		System.out.println("BB");
->>>>>>> branch 'main' of https://github.com/axia911/sample
 		System.out.println("Hi");
 		System.out.println("뭐해야됨");
 	}
