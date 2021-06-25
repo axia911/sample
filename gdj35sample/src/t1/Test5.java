10078ee3042d0d941507d5b2571d9213e6ae0e49
주건정 <JuGeonjeong>
2021-06-25 15:21:42 +0900

    1

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -4,17 +4,5 @@ public class Test5 {
 	public static void main(String[] args) {
 	System.out.println("침투");
 	}
-<<<<<<< HEAD
-	
-	public static void gugu()
-	{
-		for(int i = 1; i <= 9; i++)
-		{
-			System.out.println("2 * "+i+"= "+2*i);
-		}
-	}
-	System.out.println("유투브 : 진해군함제 블루스틱 검색  <<<< JB공연직캠");
+
 }
-=======
-}
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
