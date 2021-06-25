fadd1178b0fe7efbc99c42a702ae01c7bc6c9f21
권재현 <wogus21694@gmail.com>
2021-06-25 15:37:29 +0900

    ㅋ

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -4,13 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		
-		
-		System.out.println("그만 침투하세요..ㅠㅠ");
-		System.out.println("그만 침투하세요..ㅠㅠ");
-		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
-		System.out.println("아이유");
-		System.out.println("제발");
-		System.out.println("제발");
+		System.out.println("새로시작");
 		
 	}
 }
