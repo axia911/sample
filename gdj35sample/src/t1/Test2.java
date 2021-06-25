e9befdc1df6820daa5ec9121a87f6a0243a35dc1
주건정 <JuGeonjeong>
2021-06-25 15:22:47 +0900

    123

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -2,36 +2,10 @@ package t1;
 
 public class Test2 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("나는 바람");
 		
 		System.out.println("잠시 들렸다 가요 ^^ ;;");
 		System.out.println("이번이다!!!");
 
-<<<<<<< HEAD
-		System.out.println("건정아 ㅎㅇ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-		System.out.println("건정아 ");
-=======
-		
-
-		
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
 	}
 }
