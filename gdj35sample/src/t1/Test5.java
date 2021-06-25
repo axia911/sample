9c028eb27c29d8320f75919c3456fc4e391f3cb0
LeeInBok <bbock1214@gmail.com>
2021-06-25 13:50:53 +0900

    add function 'gugu()'

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -5,5 +5,14 @@ public class Test5 {
 		System.out.println("아아 마이크 테스트!!!");
 		
 		System.out.println("Is our team pushing something on?");
+		
+		gugu();
+	}
+	public static void gugu()
+	{
+		for(int i = 1; i <= 9; i++)
+		{
+			System.out.println("2"+"*"+i+"="+i*2);
+		}
 	}
 }
