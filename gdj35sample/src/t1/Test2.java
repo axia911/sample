3c7c8cecbc33f02dea6e39546efbded5251c1933
이혁준 <xncbv2696121@gmail.com>
2021-06-25 13:50:48 +0900

    금융

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -4,6 +4,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("abcd");
-		System.out.println("테스트테스트");
+		System.out.println("테스트테스트1");
 	}
 }
