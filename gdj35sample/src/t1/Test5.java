dfe3cbcae6f56629d8701f4b0dc1f8c051a90946
LeeInBok <bbock1214@gmail.com>
2021-06-25 15:11:44 +0900

    add function gugu()

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -14,5 +14,13 @@ public class Test5 {
 		System.out.println("올린다 진짜로 진짜");
 		System.out.println("바람 2");
 		System.out.println("!!!");
+		gugu();
+	}
+	public static void gugu()
+	{
+		for(int i = 1; i <= 9; i++)
+		{
+			System.out.println("2 * "+i+"= "+2*i);
+		}
 	}
 }
