7d97652daa838297c85a032e4eb05ed00f87c37a
이영민 <omoha@naver.com>
2021-06-25 15:18:15 +0900

    영민

--- gdj35sample/src/t1/Test4.java
+++ gdj35sample/src/t1/Test4.java
@@ -4,5 +4,13 @@ public class Test4 {
 	public static void main(String[] args) {
                                                                      
 		System.out.println("깨끗");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		System.out.println("더럽히겠다.");
+		
 	}                                                                                                   
 }
