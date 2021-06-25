0e620ec84d09082fbbb03c7952b81fe852c0928a
방민규 <pbb0636@gmail.com>
2021-06-25 14:48:32 +0900

    11

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -2,10 +2,19 @@ package t1;
 
 public class Test2 {
 	public static void main(String[] args) {
+		System.out.println("금융조닷!!");
+		System.out.println("김도헌!!");
 		// TODO Auto-generated method stub
 		System.out.println("abcd");
 		System.out.println("테스트테스트1");
+		System.out.println("왜 안되는거냐 혁준아");
 		System.out.println("괴도 키드 왔다감 ㅋㅋㅋㅋ");
+		System.out.println("이게 나야 혁준상");
+		System.out.println("제가보이시나요?");
 		System.out.println("아이언맨 왔다감!! ㅋㅋㅋㅋ");
+		System.out.println("헤이~");
+		System.out.println("선생님들 ?");
+		System.out.println("선생님들 아무도없나요 ?");
+		System.out.println("김도헌입니다");
 	}
 }
