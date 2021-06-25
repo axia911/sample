63832ee26e48bb0de95198baf6fd810afce6dc00
LeeInBok <bbock1214@gmail.com>
2021-06-25 14:09:11 +0900

    ???

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -4,7 +4,7 @@ public class Test5 {
 	public static void main(String[] args) {
 		System.out.println("아아 마이크 테스트!!!");
 		
-		System.out.println("Is our team pushing something on?");
+		System.out.println("Is our team pushing something on....?");
 		
 		gugu();
 	}
