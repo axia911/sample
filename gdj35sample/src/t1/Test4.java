9c50dd27009e2065661d7b1efeb29baff7b3b3d2
정희두 <jhd7130@gmail.com>
2021-06-25 14:12:31 +0900

    ㅇㄻㄶㅎ

--- gdj35sample/src/t1/Test4.java
+++ gdj35sample/src/t1/Test4.java
@@ -14,6 +14,6 @@ public class Test4 {
 		System.out.println("여러분 어딨죠");
 		System.out.println("우오앙");
 		System.out.println("어렵당");
-		System.out.println("영민아 문법 맞춰야지");
+		System.out.println("영민아 문법 맞춰야지!!!!");
 	}
 }
