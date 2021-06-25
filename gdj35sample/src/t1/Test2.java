10b0f01124ce1a5b21e6720bc04c4f129f38598d
주건정 <JuGeonjeong>
2021-06-25 15:44:56 +0900

    1

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -4,18 +4,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		
-<<<<<<< HEAD
-		
-		System.out.println("그만 침투하세요..ㅠㅠ");
-		System.out.println("그만 침투하세요..ㅠㅠ");
-		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
-		System.out.println("아이유");
-		System.out.println("제발");
-		System.out.println("제발");
-		System.out.println("재시작");
-=======
-		System.out.println("새로시작");
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
 		
 	}
 }
