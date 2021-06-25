ea112c2444604198f5bcf5207bf28db721db2a6e
유시연 <syou0296@naver.com>
2021-06-25 14:01:30 +0900

    asdf

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -2,16 +2,5 @@ package t1;
 
 public class Test2 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("안녕 나는 시여니야");
-=======
-		// TODO Auto-generated method stub
-		System.out.println("abcd");
-		System.out.println("테스트테스트1");
-<<<<<<< HEAD
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
-=======
-		System.out.println("괴도 키드 왔다감 ㅋㅋㅋㅋ");
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
 	}
 }
--- gdj35sample/src/t1/Test3.java
+++ gdj35sample/src/t1/Test3.java
@@ -3,8 +3,7 @@ package t1;
 public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("왜 월요일 교육 대면인가요??");
-		System.out.println("왜 안되");
-		System.out.println("왜 안되2");
-		System.out.println("왜 안되3");
+		System.out.println("얘들아 점심은 맛있게 먹었니??");
+		System.out.println("난 차돌된장 먹었어 다들 뭐 먹었어??");
 	}
 }
