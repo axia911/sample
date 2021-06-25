b98866a135719a1960dfd21fac74b42b18c96a75
권재현 <wogus21694@gmail.com>
2021-06-25 15:19:36 +0900

    침투

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -2,16 +2,6 @@ package t1;
 
 public class Test5 {
 	public static void main(String[] args) {
-	System.out.println("지나가는 나그네입니다.");
-		gugu();
+	System.out.println("침투");
 	}
-	
-	public static void gugu()
-	{
-		for(int i = 1; i <= 9; i++)
-		{
-			System.out.println("2 * "+i+"= "+2*i);
-		}
-	}
-	
 }
