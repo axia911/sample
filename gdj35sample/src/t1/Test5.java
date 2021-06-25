426c21a69d64a7683523de3792fe88949c74f00c
최정민 <dndbejtk@naver.com>
2021-06-25 15:28:19 +0900

    .

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -5,10 +5,6 @@ public class Test5 {
 	System.out.println("침투");
 		System.out.println("어떻게!!");
 		System.out.println("왜!!");
-	}
-<<<<<<< HEAD
 
+	}
 }
-=======
-}
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
