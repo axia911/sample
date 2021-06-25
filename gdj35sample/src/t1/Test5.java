a00e208208bf6b2d25d0fae206a4d8cbda8e1fe5
최정민 <dndbejtk@naver.com>
2021-06-25 15:36:09 +0900

    .

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -2,17 +2,7 @@ package t1;
 
 public class Test5 {
 	public static void main(String[] args) {
-	System.out.println("침투");
-		System.out.println("어떻게!!");
-		System.out.println("왜!!");
-		System.out.println("나도 병합햅고싶다!");
-		System.out.println("유투브 스타 JM");
-<<<<<<< HEAD
-		System.out.println("진해군함제 블루스틱 많이 사랑해주세요~");
-		
-=======
-		System.out.println("나도 병합!!!");
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
+
 	}
 
 
