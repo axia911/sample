d1fb99223b157425774128b4e238a15ed89ec1cb
박호준 <@DESKTOP-K0B7MSH>
2021-06-25 13:58:27 +0900

    수정

--- gdj35sample/src/t1/Test1.java
+++ gdj35sample/src/t1/Test1.java
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 package t1;
 
-public class Test1 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("1234123zxczxczxczxczxxz4");
-	}
-}
-=======
-package t1;
-
-public class Test1 {
-	public static void main(String[] args) {
-		System.out.println("아아 마이크 테스트");
-
-	}
-=======
-package t1;
 
 public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("moo~ya~ho");
+			System.out.println("아아 마이크 테스트");
 	}
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
 }
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
+
