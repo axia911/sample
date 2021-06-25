3196f91613b61662963b7cae65d80952a6f2ce5f
권재현 <wogus21694@gmail.com>
2021-06-25 15:20:47 +0900

    ㅋㅋ

--- gdj35sample/src/t1/Test4.java
+++ gdj35sample/src/t1/Test4.java
@@ -3,6 +3,6 @@ package t1;
 public class Test4 {
 	public static void main(String[] args) {
                                                                      
-		System.out.println("깨끗");
+		System.out.println("희두형 핸드폰하고있어요");
 	}                                                                                                   
 }
