7e3fcdd57418cacee8718cd16434a1e5b4975b89
박호준 <mmmmasd112@naver.com>
2021-06-25 15:06:27 +0900

    머지

--- gdj35sample/src/t1/Test1.java
+++ gdj35sample/src/t1/Test1.java
@@ -8,12 +8,6 @@ public class Test1 {
 		System.out.println("Hi");
 		System.out.println("test");
 		System.out.println("ㅜㅠ");
-	
-		System.out.println("Hi2");
-		System.out.println("Hi3");
 		System.out.println("ㅡㅡ");
-		System.out.println("test1232");
-		System.out.println("test1232");
-		System.out.println("test1232");
 	}
 }
