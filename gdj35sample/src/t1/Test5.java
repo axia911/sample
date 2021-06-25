a0c490062a8943e7c4092766ed392818f2df1ff1
권재현 <wogus21694@gmail.com>
2021-06-25 14:14:16 +0900

    누구일까요

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -7,5 +7,6 @@ public class Test5 {
 		System.out.println("Is our team pusing something on?");
 		System.out.println("안녕");
 		System.out.println("괴도키드 왔다감 ㅋㅋ");
+		System.out.println("캡틴아메리카 왔다감 ㅋㅋ");
 	}
 }
