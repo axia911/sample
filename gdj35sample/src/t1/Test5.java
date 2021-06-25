a1ef3430964511588d759786bb80333d6b493b50
김현 <gus5686@naver.com>
2021-06-25 15:27:43 +0900

    침투@

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -6,9 +6,7 @@ public class Test5 {
 		System.out.println("어떻게!!");
 		System.out.println("침투다!!");
 	}
-<<<<<<< HEAD
+
 
 }
-=======
-}
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
+
