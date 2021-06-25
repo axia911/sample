e28bb47dc59bf0aaf0a77e01a88a662d3ae36c23
박수빈 <qkrtnqlsdla@naver.com>
2021-06-25 13:53:13 +0900

    ㅂ자기바깁ㅈ가ㅣ

--- gdj35sample/src/t1/Test3.java
+++ gdj35sample/src/t1/Test3.java
@@ -5,5 +5,8 @@ public class Test3 {
 		System.out.println("왜 월요일 교육 대면인가요??");
 		System.out.println("왜 안되");
 		System.out.println("왜 안되2");
+
+		System.out.println("박수빈2");
+		System.out.println("박수빈2");
 	}
 }
