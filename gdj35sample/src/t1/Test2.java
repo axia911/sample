a1ebf33788ec5fcda8bf9ec8b112634912e9f28c
김도헌 <hun6791@naver.com>
2021-06-25 14:29:36 +0900

    제발제발

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -34,6 +34,7 @@ public class Test2 {
 		System.out.println("김도헌입니다");
 		System.out.println("오우쉣 ~~~~~~~~ 무냐구~~~~~~~~~~~~~");
 		System.out.println("우리조 뭐냐 계속터져 ㅋㅋㅋㅋ 누구야 범인이!!");
+		System.out.println("제발제발제발");
 >>>>>>> branch 'main' of https://github.com/axia911/sample.git
 	}
 }
