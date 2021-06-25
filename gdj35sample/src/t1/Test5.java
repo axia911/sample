a215ad9ea131d1d427e2e00d41c87f4f4986ea38
최정민 <dndbejtk@naver.com>
2021-06-25 13:45:47 +0900

    커커커커커 커밋~!

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -2,7 +2,12 @@ package t1;
 
 public class Test5 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] a = {1, 2, 3, 4, 5};
+		
+		for(int i = 0 ; i <= 4 ; i++) {
+			System.out.println(a);
+		}
 
 	}
 }
