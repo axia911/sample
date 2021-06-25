ca4773301cb3a56893cf3c120ead8399ca426b5a
장원혁 <seiou@naver.com>
2021-06-25 15:34:01 +0900

    드로아바바바

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -3,12 +3,14 @@ package t1;
 public class Test2 {
 	public static void main(String[] args) {
 		
-		ㅁㄴㅇㅁㅇ;
+		
 		
 		
 		System.out.println("그만 침투하세요..ㅠㅠ");
 		System.out.println("그만 침투하세요..ㅠㅠ");
 		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
-		}}]]]]]]]]]]]]]]
+	
+		
+		
 	}
 }
