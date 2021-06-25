87da2727ce4eaa3a96abc8777d67bcb86a1e0adb
LeeInBok <bbock1214@gmail.com>
2021-06-25 15:23:25 +0900

    restore previous code and add function comment

--- gdj35sample/src/t1/Test5.java
+++ gdj35sample/src/t1/Test5.java
@@ -1,7 +1,21 @@
 package t1;
 
 public class Test5 {
-	public static void main(String[] args) {
-	System.out.println("침투");
+ 	public static void main(String[] args) {
+ 		gugu();
+ 		comment();
+ 	}
+	
+	public static void gugu()
+	{
+		for(int i = 1; i <= 9; i++)
+		{
+			System.out.println("2 * "+i+"= "+2*i);
+		}
+	}
+	
+	public static void comment()
+	{
+		System.out.println("어서오세요");
 	}
 }
