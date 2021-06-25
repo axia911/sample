7797a6b4e71c1a8497f25e150fc0cf3e24513a70
happy <happy@DESKTOP-K0B7MSH>
2021-06-25 14:03:26 +0900

    수정

--- gdj35sample/src/t1/Test1.java
+++ gdj35sample/src/t1/Test1.java
@@ -6,7 +6,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("mingyu ");
 
-
+		System.out.println("zxczxczxczxcxzzxc");
 
 		System.out.println("아아 마이크 테스트");
 
