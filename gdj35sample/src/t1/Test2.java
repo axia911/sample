68385b0ed5d26b48260307bcecb03ad41e9aa87b
정희두 <jhd7130@gmail.com>
2021-06-25 15:06:59 +0900

    ㅇㄹㄹㄹ

--- gdj35sample/src/t1/Test2.java
+++ gdj35sample/src/t1/Test2.java
@@ -3,7 +3,7 @@ package t1;
 public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("나는 바람");
+		System.out.println("디도스 공격");
 
 	}
 }
