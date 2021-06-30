82d2ca987e3a08d6c3d20e934b93bb919a9b120d
강사 <axia911@gmail.com>
2021-06-30 18:08:54 +0900

    test

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,16 +3,5 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("다시");
-
-
-		System.out.println("다시");
-		System.out.println("=정리=");
-		System.out.println("왜 다 없어졌어 ?");
-		System.out.println("테스트 완료");
-	
-		System.out.println("장현나씨 아무것도 안한대요");
-		System.out.println("테스트중이다");
-		System.out.println("bogo");
-		System.out.println("테스트중이다dlfakdi");
 	}
 }
