6577d9dea1b11f0bd00e6fbfaec7949aa138486b
임지혜 <akdi22@hanmail.net>
2021-09-30 11:59:02 +0900

    이제된다

--- gdj37sample/src/gdj37sample/TestJina.java
+++ gdj37sample/src/gdj37sample/TestJina.java
@@ -4,6 +4,8 @@ public class TestJina {
 
 	public static void main(String[] args) {
 		System.out.println("Bye~");
+		System.out.println("hi~");
+		System.out.println("hello~");
 
 	}
 
--- gdj37sample/src/gdj37sample/TestO.java
+++ gdj37sample/src/gdj37sample/TestO.java
@@ -4,6 +4,10 @@ public class TestO {
 
 	public static void main(String[] args) {
 		System.out.println("HelloHello");
+		System.out.println("He");
+		System.out.println("He");
+		System.out.println("He");
+		System.out.println("HelloHello");
 	}
 
 }
