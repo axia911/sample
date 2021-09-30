a4da8b5664d2bd094e574fce626fe100df0147fa
choi2u <font_e0126@naver.com>
2021-09-30 12:20:03 +0900

    cc

--- gdj37sample/src/gdj37sample/Test2.java
+++ gdj37sample/src/gdj37sample/Test2.java
@@ -1,9 +1,9 @@
 package gdj37sample;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("어떻게 하는거지");
+		System.out.println("보여주기식");
 	}
 
 }
--- gdj37sample/src/gdj37sample/TestO.java
+++ gdj37sample/src/gdj37sample/TestO.java
@@ -1,9 +1,9 @@
 package gdj37sample;
 
-public class Test {
+public class TestO {
 
 	public static void main(String[] args) {
-		System.out.println("어떻게 하는거지");
+		System.out.println("HelloHello");
 	}
 
 }
