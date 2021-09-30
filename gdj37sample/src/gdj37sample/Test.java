4b57559b7ad2fd94af16fc56c836591fb0aca66f
지나 <yeonjn95@naver.com>
2021-09-30 12:01:06 +0900

    ㅋ

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -5,7 +5,13 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Bye~");
 		System.out.println("�ÿ�");
-		System.out.println("추가");
+		System.out.println("Bye~");
+		
+		System.out.println("Bye~");
+		System.out.println("Bye~");
+		System.out.println("Bye~");
+		System.out.println("Bye~");
+		System.out.println("Bye~");
 	}
 
 }
