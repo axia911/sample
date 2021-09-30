8274e62369686c80fe12b87e24f4b2a873b662da
권혁준 <hg1243@naver.com>
2021-09-30 22:34:32 +0900

    내용 수정해봄

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -3,14 +3,7 @@ package gdj37sample;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello2");
-		System.out.println("안녕!");
-
-		System.out.println("이 소스코드는 제꺼입니다.");
-		System.out.println("Bye~");
-		System.out.println("연습");
-		System.out.println("어떻게 하는거지");
-		System.out.println("Bye~");
+		System.out.println("오후10시33분");
 	}
 
 }
