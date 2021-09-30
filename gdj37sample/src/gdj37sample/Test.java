ded53f1a1e0c11ab7c7dd6b70906fc6aa534b564
choi2u <font_e0126@naver.com>
2021-09-30 11:52:34 +0900

    어떻게?

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello2");
-		System.out.println("이번역은 종착역입니다 내리실 문은 없습니다.");
+		System.out.println("어떻게 하는거지");
 	}
 
 }
