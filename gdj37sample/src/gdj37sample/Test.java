7d859e46f32a3d53b010e0b2e9955b03dbd70e4c
pjins <pjins9787@gmail.com>
2021-09-30 12:10:31 +0900

    ??

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -3,12 +3,8 @@ package gdj37sample;
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello2");
 		System.out.println("ì•ˆë…•!");
-=======
-		System.out.println("½Ã¿ø");
->>>>>>> branch 'main' of https://github.com/axia911/sample.git
 	}
 
 }
