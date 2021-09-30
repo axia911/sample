d301e657f542d45c7b45894c3427db78f2cf0943
종인 <jjonnin.works@gmail.com>
2021-09-30 12:04:25 +0900

    ㅎㅇ

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -3,13 +3,7 @@ package gdj37sample;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("연습");
-		System.out.println("Hello3333333332");
-		System.out.println("Hello22");
-		System.out.println("Bye~");
-		System.out.println("�ÿ�");
-		System.out.println("추�?");
-		System.out.println("�𸣰ڴ� ������");
+		System.out.println("hohoho");
 	}
 
 }
