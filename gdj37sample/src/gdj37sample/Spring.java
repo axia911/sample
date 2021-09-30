4c2c8836a2ef6f956387f00e775fa67896b68700
임지혜 <akdi22@hanmail.net>
2021-09-30 12:02:06 +0900

    테스틑

--- gdj37sample/src/gdj37sample/Spring.java
+++ gdj37sample/src/gdj37sample/Spring.java
@@ -4,6 +4,14 @@ public class Spring {
 	public static void main(String[] args) {
 		System.out.println("업로드 테스트");
 		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
+		System.out.println(" 테스트 테스트 테스트 테스트 ");
 		
 		
 	}
