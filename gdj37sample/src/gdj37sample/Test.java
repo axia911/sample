99565c2ad5d5f8389b21caa1c0dbbd5a214d231f
유혜리 <tgmary09@gmail.com>
2021-09-30 12:03:00 +0900

    충돌가보자고

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -3,6 +3,7 @@ package gdj37sample;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("충돌나보게해보자");
 		System.out.println("Hello3333333332");
 		System.out.println("Hello22");
 		System.out.println("Bye~");
