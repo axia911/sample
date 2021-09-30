cb82a95b76a9a68ab8beb41bd8eb65ccbaa66117
pjins <pjins9787@gmail.com>
2021-09-30 12:20:57 +0900

    머야

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -5,10 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello2");
 		System.out.println("안녕!");
+
 		System.out.println("이 소스코드는 제꺼입니다.");
-
-		System.out.println("hohoho");
-
-
 	}
+
 }
