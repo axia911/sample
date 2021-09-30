4763882aeecd828e84c09d5e09bbce6cb4e9c6c6
이혜주 <haejulee.hjl@gmail.com>
2021-09-30 11:51:46 +0900

    올려볼까?

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -1,9 +1,10 @@
-package gdj37sample;
+ package gdj37sample;
 
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello2");
+		System.out.println("Pikachu pika pika");
 	}
 
 }
