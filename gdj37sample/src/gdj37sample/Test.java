0566229232324cb827a9d99da9c2b0fd19d0a021
임지혜 <akdi22@hanmail.net>
2021-09-30 12:07:14 +0900

    ㅇㅇㅇㅇㅇㅇ

--- gdj37sample/src/gdj37sample/Test.java
+++ gdj37sample/src/gdj37sample/Test.java
@@ -3,8 +3,7 @@ package gdj37sample;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Bye~");
-		System.out.println("�ÿ�");
+		System.out.println("Hello23456789");
 	}
 
 }
--- gdj37sample/src/gdj37sample/Test2.java
+++ gdj37sample/src/gdj37sample/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("보여주기식");
+		System.out.println("가나다라마바사");
+		
 	}
 
 }
